a7ba1c48710b107c31a5a539027fa632391dfd22
markcalendario <markcalendario@gmail.com>
2025-03-04 11:54:18 +0800

    feat: add github link to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,11 +2,18 @@ import LinkButton from "../Buttons/LinkButton/LinkButton.jsx";
 import styles from "./Navbar.module.scss";
 
 export default function Navbar() {
+  const githubLink =
+    "https://github.com/markcalendario/thesis-hardbind-cover-generator";
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.container}>
         <div className={styles.wrapper}>
-          <LinkButton className={styles.githubBtn}>GitHub</LinkButton>
+          <LinkButton
+            href={githubLink}
+            className={styles.githubBtn}>
+            GitHub
+          </LinkButton>
         </div>
       </div>
     </nav>
